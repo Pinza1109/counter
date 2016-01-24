da14ac1755bfe9a720a3686ca19998967cfdaed9
Pinza1109 <yagi@region-umui.co.jp>
2016-01-25 01:03:21 +0900

    Add New

---
+++ Assets/countercont.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class countercont : MonoBehaviour {
+	public InputField textdisp;
+    public Text numdisp;
+	// Use this for initialization
+	void Start () {
+//        num = 0;
+//        numdisp.text = num.ToString();
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+	public int num = 0;
+    public void upbuttun()
+    {
+        Debug.Log("UP");
+        num++;
+        numdisp.text = num.ToString();
+    }
+    public void dwbuttun()
+    {
+        Debug.Log("DOWN");
+        if (num != 0) {
+			num--;
+		}
+		numdisp.text = num.ToString();
+    }
+	public void setValues(string iText, string iNum){
+		num = int.Parse (iNum);
+		numdisp.text = num.ToString();
+		textdisp.text = iText;
+	}
+}
---
+++ Assets/test01.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class test01 : MonoBehaviour {
+	public countercont[] countlist;
+	// Use this for initialization
+	void Start () {
+		loadValue ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void buttun()
+    {
+        Debug.Log("START");
+		string Value = "";
+		for (int i=0; i<countlist.Length; i++) {
+			Debug.Log(countlist[i].textdisp.text+"="+countlist[i].num);
+			Value += countlist[i].textdisp.text+"="+countlist[i].num +"\n";
+		}
+		sendMail (Value);
+    }
+	public void clearValue()
+	{
+		for (int i=0; i<countlist.Length; i++) {
+			Debug.Log(countlist[i].textdisp.text+"="+countlist[i].num);
+			PlayerPrefs.SetString(countlist[i].gameObject.name,
+			                      countlist[i].textdisp.text+",0"); 
+			
+		}
+		PlayerPrefs.Save ();
+		loadValue();
+
+	}
+	private void saveValue()
+	{
+		for (int i=0; i<countlist.Length; i++) {
+			Debug.Log(countlist[i].textdisp.text+"="+countlist[i].num);
+			PlayerPrefs.SetString(countlist[i].gameObject.name,
+			                      countlist[i].textdisp.text+","+countlist[i].num); 
+
+		}
+		PlayerPrefs.Save ();
+
+	}
+	private void loadValue()
+	{
+		for (int i=0; i<countlist.Length; i++) {
+			string value=PlayerPrefs.GetString(countlist[i].gameObject.name,",0");
+			string[] values = value.Split(',');
+			if (values.Length == 2){
+				countlist[i].setValues(values[0],values[1]);
+			}
+
+		}
+
+	}
+
+	void OnApplicationPause(bool iPause){
+		if (iPause) {
+			Debug.Log("SAVE");
+			saveValue ();
+		} else {
+			loadValue ();
+			Debug.Log("LOAD");
+		}
+	}
+	void sendMail(string iValue) {
+		string email    =   "yagi@region-umui.co.jp";
+		string subject  =   "計測結果";
+		string body     =   "計測結果\n"+ iValue;
+		
+		//エスケープ処理
+		body    =System.Uri.EscapeDataString(body);
+		subject =System.Uri.EscapeDataString(subject);
+		
+		Application.OpenURL("mailto:" + email + "?subject=" + subject + "&body=" + body);
+	}
+
+}
